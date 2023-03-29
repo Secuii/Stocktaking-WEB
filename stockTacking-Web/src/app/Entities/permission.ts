42f471ea637584392b1afa05714feb595a65b398
RaulCampos1990 <127895174+RaulCampos1990@users.noreply.github.com>
2023-03-29 22:20:44 +0200

    Create permission entity
    Crear entidad permisos

---
+++ stockTacking-Web/src/app/Entities/permission.ts
@@ -0,0 +1,13 @@
+export class permission 
+{
+    public id: number;
+    public name: String;
+    public description: String;
+
+    constructor() 
+    {
+        this.id = -1;
+        this.name = "";
+        this.description = "";
+    }
+}
