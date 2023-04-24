9ac036cff8381da5a76b7645d08289f20acecf2a
victorCEV <127894230+victorCEV@users.noreply.github.com>
2023-04-24 18:48:01 +0200

    Card del componente permissions, HTM + TS

--- Stocktaking-Web/src/app/components/Permission/card-permission-component/card-permission-component.component.ts
+++ Stocktaking-Web/src/app/components/Permission/card-permission-component/card-permission-component.component.ts
@@ -1,10 +1,76 @@
-import { Component } from '@angular/core';
+import { permission } from './../../../entities/permission';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-permission-component',
   templateUrl: './card-permission-component.component.html',
   styleUrls: ['./../../components.css']
 })
-export class CardPermissionComponentComponent {
+export class CardPermissionComponentComponent implements OnInit{ 
+
+  /*
+     Variales:
+ */
+     public permissions: permission;
+     public idPermissions: number;
+ /*
+     Constructor
+ */
+ constructor(private router: Router,
+             private activatedRoute: ActivatedRoute)
+ {
+     this.permissions= new permission();
+     this.idPermissions = 0;
+ }
+
+ 
+ /*
+     Métodos implementados de interfaces:
+ */
+ ngOnInit(): void 
+ {
+     this.activatedRoute.params.subscribe
+     (
+         params => 
+         {
+             this.idPermissions = params['id'];
+         }
+     );
+
+     this.getPermissionsFromIdService();
+ }
+
+/*
+   Métodos de enrutamiento
+*/
+
+   public mainPageBtn() {
+     this.router.navigateByUrl('/main');
+   }
+ 
+   public showUsersBtn() {
+     // Ir a la página de detalle del id
+    // this.router.navigate(['users', id]);
+   }
+
+
+
+ /*
+     Método getMembershipFromIdService:
+         Entradas: Ninguna (El id lo pilla del this.membership)
+         Objetivo: petición al servicio de supplier a construir por id.
+         Salidas: Booleana. (true: Si se ha conseguido | false: Si no se ha conseguido)
+ */
+   private getPermissionsFromIdService(): void
+   {
+       /*this.membershipService.readAPermissions(this.idPermissions).subscribe(
+           response => 
+           {
+               this.permissions = response;
+           }
+       )*/
+   }
 
 }
+
