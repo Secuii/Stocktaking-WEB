c2b3b23606cafd637026aa55cfa02d9e5a1f8963
RaulCampos1990 <127895174+RaulCampos1990@users.noreply.github.com>
2023-05-14 11:37:52 +0200

    Attributes Page component, module y html

--- Stocktaking-Web/src/app/pages/attribute-page/attribute-page.component.ts
+++ Stocktaking-Web/src/app/pages/attribute-page/attribute-page.component.ts
@@ -1,13 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { Attribute } from 'src/app/entities/attribute';
+import { StatusPage } from 'src/app/enums/enum-status-page';
 
 @Component
-(
-  {
-    selector: 'app-attribute-page',
-    templateUrl: './attribute-page.component.html',
-    styleUrls: ['../pages.css']
-  }
-)
-export class AttributePageComponent {
+  (
+    {
+      selector: 'app-attribute-page',
+      templateUrl: './attribute-page.component.html',
+      styleUrls:
+        [
+          '../pages.css'
+        ]
+    }
+  )
+export class AttributePageComponent implements OnInit, OnDestroy {
+  /*
+    Variables
+  */
+  public pageTittle: string;
+  public statusPage: StatusPage;
+  public attributeSelected?: Attribute;
 
+  /*
+    Constructor
+  */
+  constructor() {
+    this.pageTittle = "Página de Atributos";
+    this.statusPage = StatusPage.ReadAll;
+    this.attributeSelected = undefined;
+  }
+
+  /*
+    Métodos eventuales
+  */
+  ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+  }
+
+  /*
+    Métodos propios
+  */
+  public changeStatusPage(newStatusPage: StatusPage): void {
+    this.statusPage = newStatusPage;
+  }
+
+  public selectAttribute(newAttributeSelected: Attribute) {
+    this.attributeSelected = newAttributeSelected;
+  }
 }
