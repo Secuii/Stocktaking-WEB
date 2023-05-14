4d200f1c2bd48f1d6db3efac7b701182957367bc
RaulCampos1990 <127895174+RaulCampos1990@users.noreply.github.com>
2023-05-14 11:37:30 +0200

    Permission Page component, module y html
    Creación del Page de permission

--- Stocktaking-Web/src/app/pages/permission-page/permission-page.component.ts
+++ Stocktaking-Web/src/app/pages/permission-page/permission-page.component.ts
@@ -1,13 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Permission } from 'src/app/entities/permission';
+import { StatusPage } from 'src/app/enums/enum-status-page';
 
 @Component
-(
-  {
-    selector: 'app-permission-page',
-    templateUrl: './permission-page.component.html',
-    styleUrls: ['../pages.css']
-  }
-)
-export class PermissionPageComponent {
+  (
+    {
+      selector: 'app-permission-page',
+      templateUrl: './permission-page.component.html',
+      styleUrls:
+        [
+          '../pages.css'
+        ]
+    }
+  )
+export class PermissionPageComponent implements OnInit, OnDestroy {
+  /*
+    Variables
+  */
+  public pageTittle: string;
+  public statusPage: StatusPage;
+  public permissionSelected?: Permission;
 
+  /*
+    Constructor
+  */
+  constructor() {
+    this.pageTittle = "Página de Permisos";
+    this.statusPage = StatusPage.ReadAll;
+    this.permissionSelected = undefined;
+  }
+
+  /*
+    Métodos eventuales
+  */
+  ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+  }
+
+  /*
+    Métodos propios
+  */
+  public changeStatusPage(newStatusPage: StatusPage): void {
+    this.statusPage = newStatusPage;
+  }
+
+  public selectPermission(newPermissionSelected: Permission) {
+    this.permissionSelected = newPermissionSelected;
+  }
 }
