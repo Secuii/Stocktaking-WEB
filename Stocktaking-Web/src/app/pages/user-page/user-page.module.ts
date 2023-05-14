135734f6582946dbe3494dc46297b74dc634d19c
RaulCampos1990 <127895174+RaulCampos1990@users.noreply.github.com>
2023-05-14 11:36:46 +0200

    User Page component, module y html
    Creación del componente

--- Stocktaking-Web/src/app/pages/user-page/user-page.module.ts
+++ Stocktaking-Web/src/app/pages/user-page/user-page.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { UserPageComponent } from './user-page.component';
+import { ListUserComponentModule } from 'src/app/components/User/list-user-component/list-user-component.module';
+import { CardUserComponentModule } from 'src/app/components/User/card-user-component/card-user-component.module';
+import { UpdateUserComponentModule } from 'src/app/components/User/update-user-component/update-user-component.module';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 
 @NgModule({
-  declarations: 
-  [
-    UserPageComponent
-  ],
-  imports: 
-  [
-  ],
-  exports: 
-  [
-    UserPageComponent
-  ]
+  declarations:
+    [
+      UserPageComponent
+    ],
+  imports:
+    [
+      RouterModule,
+      CommonModule,
+      ListUserComponentModule,
+      CardUserComponentModule,
+      UpdateUserComponentModule
+    ],
+  exports:
+    [
+      UserPageComponent
+    ]
 })
 export class UserPageModule { }
